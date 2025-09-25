fca7648b5d0c345a938318d7820d42dd37599d99
yunjin25-hue <yunjin25@sookmyung.ac.kr>
2025-09-25 16:15:08 +0900

    project5

--- week04.c
+++ week04.c
@@ -3,18 +3,23 @@
 
 int main(int argc, char *argv[])
 {
-  //실습4
+  //실습5
   
-  int year;
+  unsigned int x;
+  int b;
   
-  printf("input the year : ");
-  scanf("%d",&year);
+  printf("input a number : ");
+  scanf("%ui",&x);
   
-  printf("is the year %d the leap year? : %d\n",   //C언어는 가독성을 위해 한 줄 띄어도 문제 X 
-                                              year,
-                                              (year%4==0 && year%100!=0) || (year%400==0)); 
-                                              //년도가 4로 나누어 떨어지면서 100으로 나누어 떨어지지 않거나 400으로 나누어 떨어지면 윤년 
+  for(b=0; x !=0; x>>=1)
+  {
+           if(x&1)
+           {
+                  b++;
+           } 
+  }
   
+  printf("The result is : %i\n",b);
   
   system("PAUSE");	
   return 0;
